f911ef372a30e0aad21e9c207959b8486b742fe4
Dung Nguyen <dudu.nt107@gmail.com>
2020-11-19 20:01:05 -0600

    refactored codes using useState Hook

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import EmployeeTable from "./components/EmployeeTable";
+import SortData from "./components/SortData";
+import FilterData from "./components/FilterData";
+import Reset from "./components/Reset";
+import employeesArray from "./employees.json";
 
-function App (){
-    return (
-      <Wrapper>
-        <Title>Employee Directory</Title>
-        <EmployeeTable/>
-      </Wrapper>
-    );
-  }
+function App() {
+  const [employees, setEmployees] = useState(employeesArray);
+
+  return (
+    <Wrapper>
+      <Title>Employee Directory</Title>
+      <EmployeeTable employeeState={employees} />
+    </Wrapper>
+  );
+}
 
 export default App;
