f149e15e1022ad618019520af21b6467a52fbd1c
Dung Nguyen <dudu.nt107@gmail.com>
2020-11-19 20:04:24 -0600

    added codes for sorting and filtering data

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -10,9 +10,29 @@ import employeesArray from "./employees.json";
 function App() {
   const [employees, setEmployees] = useState(employeesArray);
 
+  const filterbyCook = () => {
+    setEmployees(employees.filter((employee) => employee.title === "Cook"));
+  };
+
+  const sortByName = () => {
+    setEmployees(
+      [...employees].sort(function (a, b) {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+          return -1;
+        }
+        if (a.name.toLowerCase() > b.name.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      })
+    );
+  };
+
   return (
     <Wrapper>
       <Title>Employee Directory</Title>
+      <SortData clickHandler={sortByName} />
+      <FilterData clickHandler={filterbyCook} />
       <EmployeeTable employeeState={employees} />
     </Wrapper>
   );
